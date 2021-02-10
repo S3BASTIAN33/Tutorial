55cf325ae50a896381a9734e6c476b2c134529a9
S3BASTIAN33 <sebastian.villacreces@udla.edu.ec>
2021-02-09 19:45:47 -0500

    Version 1

--- src/Controller/SumasController.php
+++ src/Controller/SumasController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ClienteRepository;
 use App\Repository\ContratosRepository;
+use phpDocumentor\Reflection\Types\Nullable;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class SumasController extends AbstractController
 {
@@ -22,40 +24,45 @@ class SumasController extends AbstractController
         print("Ingrese fechas ");
         $formulario= $this->createFormBuilder()
         ->setAction($this->generateUrl('sumabusqueda'))
-        ->add('FechaInicio', DateType::class, [
-    'widget' => 'single_text',
-])
-        ->add('FechaFinal', DateType::class, [
-    'widget' => 'single_text', 
-])
-        ->add('Buscar', SubmitType::class, ['attr'=>['class'=>'btn btn-primary']])
-        
+        ->add('FechaInicio', DateType::class, [ 'widget' => 'single_text',])
+        ->add('FechaFinal', DateType::class, ['widget' => 'single_text', ])
+        ->add('Buscar', SubmitType::class, ['attr'=>['class'=>'btn btn-primary']])      
         ->getForm();
-        
-
-       
         return $this->render('sumas/index.html.twig', ['formulario'=> $formulario->createView()]);
       
     }
+
       /**
      * @Route("/sumabusqueda", name="sumabusqueda")
      * @param Request $request
      */
     public function corebusqueda(Request $request, ContratosRepository $coRe){
    $fei=$request->request->get('form')['FechaInicio'];
- 
    $fef=$request->request->get('form')['FechaFinal'];
+   $buse= $coRe->busfe($fei,$fef);
+   $e=array();
+   $copia=$buse; 
   
+    $cant=count($copia)-1;
   
-     $em=$this->getDoctrine()->getManager();
-  
-      
-        $buse= $coRe->busfe($fei,$fef);  
-         
-    // dump($buse);die;
-     
-       return $this->render('sumas/sima.html.twig', ['buse' => $buse]);
-      
-      }
+ //foreach ($copia as $co) {
+   //  $j++;
+//}
+//$j--;
 
+    for ($i = 0; $i <= $cant; $i++) {
+        $e= $copia[$i]["clientes"];
+        for ($z = $i; $z <= $cant; $z++) {
+            if ($i!=$z && $e==$copia[$z]["clientes"]) {
+                $copia[$i]["monto"]=$copia[$i]["monto"]+$copia[$z]["monto"];        
+                 $copia[$z]["clientes"]=null;
+                 $copia[$z]["monto"]=null;
+                
+            }     
+        }
+        
+    }   
+
+      return $this->render('sumas/sima.html.twig', ['copia' => $copia]);
+      }
     }
